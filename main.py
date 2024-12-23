16bfb7e947315df83a00b580073814d2607abb75
juyeon <okk1829@naver.com>
2024-12-23 16:21:44 +0900

    update

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from scripts.fetch_weather_data import fetch_weather_data
 from scripts.visualize_weather import visualize_weather
-from scripts.aws_utils import upload_to_s3, log_to_cloudwatch
 import time
 
 API_KEY = "c80714957aff9be02fbe9ac56ac34d13"
@@ -19,13 +18,6 @@ def main():
             # 데이터 수집 및 CSV 파일 저장
             fetch_weather_data(API_KEY, city, input_file)
             
-            # S3에 데이터 업로드
-            upload_to_s3(input_file, bucket_name)
-
-            # CloudWatch에 로그 기록
-            log_message = f"{city} weather data updated successfully."
-            log_to_cloudwatch(log_group, log_streams[city], log_message)
-
         # 실시간 시각화
         visualize_weather(input_files, cities)
 
